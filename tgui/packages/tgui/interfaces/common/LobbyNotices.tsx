dd912106d4567b19db6e878af441b56dbb61ad87
Flleeppyy <flleeppyy@gmail.com>
2024-11-24 04:26:05 -0800

    stop yelling gosh daerniet

--- tgui/packages/tgui/interfaces/common/LobbyNotices.tsx
+++ tgui/packages/tgui/interfaces/common/LobbyNotices.tsx
@@ -19,29 +19,24 @@ export const LobbyNotices = (props: { notices?: LobbyNoticesType }) => {
   return (
     <>
       {filteredLobbyNotices.map((notice, index) => (
-        <NoticeBox danger>
-          <>
-            {typeof notice === 'string' ? (
-              <Box key={index + '_'}>{notice}</Box>
-            ) : (
-              // Process object with TGUI_SAFE
-              <>
-                {Array.isArray(notice.TGUI_SAFE)
-                  ? notice.TGUI_SAFE.map((notice, index) => (
-                      <Box
-                        key={index + '__'}
-                        dangerouslySetInnerHTML={{ __html: notice }}
-                      />
-                    ))
-                  : notice.TGUI_SAFE && (
-                      <Box
-                        key={index + '___'}
-                        dangerouslySetInnerHTML={{ __html: notice.TGUI_SAFE }}
-                      />
-                    )}
-              </>
-            )}
-          </>
+        <NoticeBox danger key={index}>
+          {typeof notice === 'string' ? (
+            <Box key={index + '_'}>{notice}</Box>
+          ) : Array.isArray(notice.TGUI_SAFE) ? (
+            notice.TGUI_SAFE.map((notice, index) => (
+              <Box
+                key={index + '__'}
+                dangerouslySetInnerHTML={{ __html: notice }}
+              />
+            ))
+          ) : (
+            notice.TGUI_SAFE && (
+              <Box
+                key={index + '___'}
+                dangerouslySetInnerHTML={{ __html: notice.TGUI_SAFE }}
+              />
+            )
+          )}
         </NoticeBox>
       ))}
     </>
